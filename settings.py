dc69322460a38812069f5f7b881dee3fcc0e216b
gallochri <chri@gallochri.com>
2017-10-24 13:48:21 +0200

    remote env config

--- settings.py
+++ settings.py
@@ -1,5 +1,17 @@
+import os.path
+
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+# absolute path to the location of ..env on remote server.
+enviroment = "/home/gallochri/gallobot_env"
+
+
+if os.path.isfile(".env"):
+    dotenv_path = join(dirname(__file__), '.env')
+    print("Local .env")
+else:
+    dotenv_path = enviroment
+    print("Remote .env")
+
 load_dotenv(dotenv_path, verbose=True)
