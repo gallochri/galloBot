0e9193686dda3c9dfc87a60d1e75ad816e74064b
gallochri <chri@gallochri.com>
2017-10-29 14:14:01 +0100

    start_bot script added

---
+++ start_bot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -xv
+GIT_REPO=$HOME/gogs-repositories/gallochri/gallobot.git
+GIT_CLONE=$HOME/bot/gallobot
+PYTHON_ENV=$HOME/bot/gallobot_env
+APP=${GIT_CLONE}/app.py
+
+kill $(ps aux | grep ${APP} | grep -v grep | awk '{print $2}')
+
+if [ ! -d ${PYTHON_ENV} ]; then
+	python3 -m venv ${PYTHON_ENV}
+fi
+
+. ${PYTHON_ENV}/bin/activate
+
+if [ -d ${GIT_CLONE} ]; then
+	cd ${GIT_CLONE}
+	git pull
+else
+	git clone ${GIT_REPO} ${GIT_CLONE}
+fi
+
+pip install -r ${GIT_CLONE}/requirements.txt
+python ${APP}
+exit
