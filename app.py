a514f8f4c3195e43871131de1296ef8dd3797cb7
gallochri <chri@gallochri.com>
2017-10-29 13:58:00 +0100

    Fix listusers in group chat

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def listusers(chat_id):
         return 'No user file'
     with open(usersfile) as csvfile:
         reader = csv.DictReader(csvfile)
-        bot.sendMessage(chat_id, "Authorized users (ID-username:")
+        bot.sendMessage(chat_id, "Authorized users (ID-username):")
         for row in reader:
             userlist = userlist + row['ID'] + " - " + row['Username'] + "\n"
         bot.sendMessage(chat_id, userlist)
@@ -129,7 +129,7 @@ def handle(msg):
         global active
         active = False
         bot.sendMessage(chat_id, "The bot will shutdown in 10 seconds")
-    elif command == '/listusers':
+    elif command == '/listusers' or command == '/listusers@gallo_chat_bot':
         listusers(chat_id)
     elif command != '':
         answer = chatter.reply(command)
