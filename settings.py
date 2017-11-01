492dc58959f5c60fcf93faf9e4f1881c367d3424
Christian Galeffi <chri@gallochri.com>
2017-11-01 20:55:03 +0100

    Update setting for local enviroment

--- settings.py
+++ settings.py
@@ -3,15 +3,17 @@ import os.path
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-# absolute path to the location of ..env on remote server.
-enviroment = "/home/gallochri/gallobot_env"
+# absolute path to the location of .env on remote server.
+remote_env = "/home/gallochri/gallobot_env"
+
+# file to load locally
+local_env = ".env_locale"
 
 
-if os.path.isfile(".env"):
-    dotenv_path = join(dirname(__file__), '.env')
-    print("Local .env")
+if os.path.isfile(local_env):
+    dotenv_path = join(dirname(__file__), local_env)
 else:
-    dotenv_path = enviroment
-    print("Remote .env")
+    dotenv_path = remote_env
 
+print("Enviroment: " + dotenv_path)
 load_dotenv(dotenv_path, verbose=True)
